485413c0c155030b1ab25be5a1ed663a4df838ab
Georg Müller <georgmueller@gmx.net>
2022-04-25 12:22:11 +0200

    simplify perfcnt-stats
    Do not use cc intermediate tuple, which was often only used partially.

--- src/bin/stats.rs
+++ src/bin/stats.rs
@@ -6,62 +6,47 @@ fn print_stats(year: &'static str, name: &'static str, size: usize) {
 
 fn main() {
     // 2008, 4
-    let cc = ("Bonnell core", events::BONNELL);
-    print_stats("2008", cc.0, cc.1.len());
+    print_stats("2008", "Bonnell core", events::BONNELL.len());
 
     // 2008, 4
-    let cc = ("Nehalem EP core", events::NEHALEMEP);
-    print_stats("2008", cc.0, cc.1.len());
+    print_stats("2008", "Nehalem EP core", events::NEHALEMEP.len());
 
-    let cc = ("Nehalem EX core", events::NEHALEMEX);
-    print_stats("2008", cc.0, cc.1.len());
+    print_stats("2008", "Nehalem EX core", events::NEHALEMEX.len());
 
     // 2010, 4
-    let cc = ("Westmere EP DP core", events::WESTMEREEP_DP);
-    print_stats("2010", cc.0, cc.1.len());
+    print_stats("2010", "Westmere EP DP core", events::WESTMEREEP_DP.len());
 
-    let cc = ("Westmere EP SP core", events::WESTMEREEP_SP);
-    print_stats("2010", cc.0, cc.1.len());
+    print_stats("2010", "Westmere EP SP core", events::WESTMEREEP_SP.len());
 
-    let cc = ("Westmere EX", events::WESTMEREEX);
-    print_stats("2010", cc.0, cc.1.len());
+    print_stats("2010", "Westmere EX", events::WESTMEREEX.len());
 
     // 2011, 8
-    let cc = ("SandyBridge core", events::SANDYBRIDGE);
-    print_stats("2011", "SandyBridge", cc.1.len());
+    print_stats("2011", "SandyBridge", events::SANDYBRIDGE.len());
 
     // 2011, 8
-    let cc = ("Jaketown core", events::JAKETOWN);
-    print_stats("2011", "Jaketown", cc.1.len());
+    print_stats("2011", "Jaketown", events::JAKETOWN.len());
 
     // 2012, 8
-    let cc = ("IvyBridge core", events::IVYBRIDGE);
-    print_stats("2012", "IvyBridge", cc.1.len());
+    print_stats("2012", "IvyBridge", events::IVYBRIDGE.len());
 
     // 2013, 8
-    let cc = ("IvyTown core", events::IVYTOWN);
-    print_stats("2012", "IvyTown", cc.1.len());
+    print_stats("2012", "IvyTown", events::IVYTOWN.len());
 
     // 2013, 8
-    let cc = ("Silvermont core", events::SILVERMONT);
-    print_stats("2013", "Silvermont", cc.1.len());
+    print_stats("2013", "Silvermont", events::SILVERMONT.len());
 
     // 2013, 8
-    let cc = ("Haswell core", events::HASWELL);
-    print_stats("2013", "Haswell", cc.1.len());
+    print_stats("2013", "Haswell", events::HASWELL.len());
 
     // 2013, 8
-    let cc = ("HaswellX core", events::HASWELLX);
-    print_stats("2013", "HaswellX", cc.1.len());
+    print_stats("2013", "HaswellX", events::HASWELLX.len());
 
     // 2015, 8
-    let cc = ("Broadwell core", events::BROADWELL);
-    print_stats("2015", "Broadwell", cc.1.len());
+    print_stats("2015", "Broadwell", events::BROADWELL.len());
 
     // 2015, 8
-    print_stats("2015", "Broadwell DE", cc.1.len());
+    print_stats("2015", "Broadwell DE", events::BROADWELLDE.len());
 
     // 2015, 8
-    let cc = ("Skylake core", events::SKYLAKE);
-    print_stats("2015", "Skylake", cc.1.len());
+    print_stats("2015", "Skylake", events::SKYLAKE.len());
 }
